980ab073563b6ea1a609d4dfee796cded1b59a7a
Terafol <louplangard@gmail.com>
2023-12-13 01:47:14 +0100

    Connexion plusieurs, démarage du jeu

--- game_logic.py
+++ game_logic.py
@@ -2,13 +2,13 @@ import socket
 import subprocess
 import asyncio
 import websockets
-
+import json
 
 #A faire:
 #Toute la logique du jeu dans une fonction (appeler une pièce, tourner, envoyer les modifs...)
 #L'host doit pouvoir lancer quand il veut et voir en temps réel le nombre de personnes connéctées.
 
-
+my_number=0
 async def get_local_ip():
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -33,38 +33,53 @@ def show_menu():
 def start_server(local_ip):
     subprocess.Popen(['python', 'Server.py', local_ip, '4242'], creationflags=subprocess.CREATE_NEW_CONSOLE)
 
+async def gameplay(websocket,turn):
+    global my_number
+    if(turn==my_number):
+        # on joue normalement
+    async for message in websocket:
+        data = json.loads(message)
+       
+           
+
 async def connect_to_server(local_ip):
+    global my_number
     while True:
         uri = f"ws://{local_ip}:4242"
         async with websockets.connect(uri) as websocket:
             print(f"Connecté au serveur {local_ip}:4242 !")
-
-            while True:
-                # Attendre la réponse du serveur
-                response = await websocket.recv()
-                print(f"Nouvelle information du serveur : {response}")
-
-                # Proposer de démarrer la partie ou effectuer d'autres actions en fonction de la réponse
-                # Demander à l'utilisateur s'il veut démarrer la partie
-                start_game = input("Voulez-vous démarrer la partie ? (o/n): ")
-                if start_game.lower() == "o":
-                    await websocket.send("Démarrer la partie")
-
+            async for message in websocket:
+                data = json.loads(message)
+                if(data["action"]=="yourNumber"):
+                    my_number=data["nb"]
+                if(data["action"]=="NewConnexion"):
+                    print("Nouvelle connexion, nombre de joueurs connecté: ",data["con"])
+                    start_game = input("Voulez-vous démarrer la partie ? (o/n): ")
+                    if start_game.lower() == "o":
+                        await websocket.send(json.dumps({"action":"startGame"}))
+                if(data["action"]=="startGame"):
+                    print("On commence")
                 # Vous pouvez ajouter ici d'autres actions en fonction des réponses du serveur
 
 async def join_game():
+    global my_number
     ip = input("Entrez l'adresse IP du serveur : ")
     port = input("Entrez le port du serveur : ")
-
-    uri = f"ws://{ip}:{port}"
-    async with websockets.connect(uri) as websocket:
-        print(f"Connecté au serveur {ip}:{port} !")
-
-        while True:
-            # Attendre la réponse du serveur
-            response = await websocket.recv()
-            print(f"Nouvelle information du serveur : {response}")
-
+    while True:
+        uri = f"ws://{ip}:{port}"
+        async with websockets.connect(uri) as websocket:
+            print(f"Connecté au serveur {ip}:{port} !")
+            print("")
+            print("En attente de l'host")
+            async for message in websocket:
+                data = json.loads(message)
+                if(data["action"]=="yourNumber"):
+                    my_number=data["nb"]
+                if(data["action"]=="NewConnexion"):
+                    print("Nouvelle connexion, nombre de joueurs connecté: ",data["con"])
+                if(data["action"]=="startGame"):
+                    await gameplay(websocket,data["currentPlayer"])
+                    print("bbbb")
 
 async def main():
     while True:
@@ -78,13 +93,12 @@ async def main():
             if local_ip:
                 print(f"Adresse IP locale : {local_ip}")
                 start_server(local_ip)
-                input("Appuyez sur \"Entré\" pour vous connécter")
                 await connect_to_server(local_ip)
             else:
                 print("Impossible de récupérer l'adresse IP.")
         elif choice == 2:
             # Ajouter le code pour rejoindre une partie ici
-            pass
+            await join_game()
         else:
             print("Choix invalide. Veuillez réessayer.")
 
--- server.py
+++ server.py
@@ -1,90 +1,64 @@
-#A faire:
-#Transmettre les infos des clients
-#Une serte de "mix" du programme actuel et de celui commenté
+
+
+import asyncio
+import websockets
+import json
+import random
+import sys
 
 
 
+async def handler(websocket, path):
+    global clients, player_count, required_players, start_game_sent, player_number, starting_player
+    print("Nouvelle connexion établie.")
+    clients.append(websocket)
+    player_count += 1
+    await websocket.send(json.dumps({"action":"yourNumber","nb":player_count}))
+    await clients[0].send(json.dumps({"action":"NewConnexion","con":player_count}))
+    try:
+        async for message in websocket:
+            data = json.loads(message)
+            # Traiter les messages reçus ici
+             # Vérifie l'action "setPlayers" pour définir le nombre de joueurs requis
+            if data.get("action") == "startGame":
 
-# import asyncio
-# import websockets
-# import json
-# import random
+                 # Vérifie si le nombre de joueurs requis est atteint pour démarrer le jeu
+                if not start_game_sent:
+                    # Choix aléatoire du joueur qui commence
+                    starting_player = random.randint(1, len(clients))
 
-# async def connection_handler(websocket, path):
-#     global clients, player_count, required_players, start_game_sent, player_number, starting_player
+                    # Crée un message combiné pour "startGame" et "currentPlayer" et l'envoie à tous les clients
+                    start_game_message = {
+                        "action": "startGame",
+                         "currentPlayer": starting_player
+                     }
+                    await asyncio.wait([client.send(json.dumps(start_game_message)) for client in clients])
+                    start_game_sent = True
 
-#     # Ajoute le client à la liste des clients connectés et incrémente le nombre de joueurs
-#     clients.add(websocket)
-#     player_count += 1
-
-#     # Envoie un numéro de joueur à chaque nouveau client
-#     if player_number <= required_players:
-#         await websocket.send(json.dumps({"action": "givePlayerNumber", "number": player_number}))
-#         player_number += 1
-
-#     try:
-#         async for message in websocket:
-#             data = json.loads(message)
-
-#             # Vérifie l'action "setPlayers" pour définir le nombre de joueurs requis
-#             if data.get("action") == "setPlayers" and "players" in data:
-#                 required_players = data["players"]
-
-#                 # Vérifie si le nombre de joueurs requis est atteint pour démarrer le jeu
-#                 if not start_game_sent and player_count >= required_players:
-#                     # Choix aléatoire du joueur qui commence
-#                     starting_player = random.randint(1, required_players)
-
-#                     # Crée un message combiné pour "startGame" et "currentPlayer" et l'envoie à tous les clients
-#                     start_game_message = {
-#                         "action": "startGame",
-#                         "currentPlayer": starting_player
-#                     }
-#                     await asyncio.wait([client.send(json.dumps(start_game_message)) for client in clients])
-#                     start_game_sent = True
-
-#             # Relaie tous les autres messages à tous les clients connectés
-#             await asyncio.wait([client.send(message) for client in clients if client != websocket])
-#     finally:
-#         # Décrémente le nombre de joueurs lorsque quelqu'un se déconnecte et le retire de la liste des clients
+            # Relaie tous les autres messages à tous les clients connectés
+            else:
+                await asyncio.wait([client.send(message) for client in clients if client != websocket])
+    finally:
+        print("Connexion terminée.")
+        # Décrémente le nombre de joueurs lorsque quelqu'un se déconnecte et le retire de la liste des clients
 #         player_count -= 1
 #         clients.remove(websocket)
 
-# async def start_server(ip, port):
-#     global clients, player_count, required_players, start_game_sent, player_number, starting_player
-
-#     # Initialisation des variables pour le serveur
-#     required_players = 0
-#     player_count = 0
-#     clients = set()
-#     start_game_sent = False
-#     player_number = 1
-#     starting_player = 0
-
-#     # Démarre le serveur WebSocket et attend les connexions
-#     async with websockets.serve(connection_handler, ip, port):
-#         await asyncio.Future()
-
-# # Exécute le serveur avec l'adresse IP et le port spécifiés
-# asyncio.run(start_server("172.20.10.6", 4242))
-import asyncio
-import websockets
-import sys
 
 async def server(ip, port):
+    global clients, player_count, required_players, start_game_sent, player_number, starting_player
+
+    # Initialisation des variables pour le serveur
+    required_players = 0
+    player_count = 0
+    clients = []
+    start_game_sent = False
+    player_number = 1
+    starting_player = 0
+
     async with websockets.serve(handler, ip, port):
         print(f"Serveur démarré à l'adresse {ip} sur le port {port}")
         await asyncio.Future()  # Maintien du serveur ouvert
-
-async def handler(websocket, path):
-    print("Nouvelle connexion établie.")
-
-    try:
-        async for message in websocket:
-            # Traiter les messages reçus ici
-            pass
-    finally:
-        print("Connexion terminée.")
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
